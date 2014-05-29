c5d10f7e3f7984f0be41419ffecc60dd5c94966b
Rob Hoelz <rob@hoelz.ro>
2014-05-28 22:44:57 -0500

    initial commit

---
+++ lib/Devel/Trace/Syscall.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Devel::Trace::Syscall;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A short description of Devel::Trace::Syscall
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
